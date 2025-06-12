8b864408d30db8d8e96678ee3dce6459a4961387
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-06-12 09:45:21 +0530

    Level 1 and Level 2 completed

---
+++ Level2/PurchasingPrice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class PurchasingPrice{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.print(" Unit Price is: ");
+double unitPrice=sc.nextDouble();
+System.out.print(" Quantity is: ");
+int quantity=sc.nextInt();
+double total=unitPrice*quantity;
+System.out.println("The total purchase price is INR "+total+" if the quantity "+quantity+" and unit price is INR "+unitPrice);
+}
+}
