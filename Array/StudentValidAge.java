7c94a8226d0c10d33ddf387ab0e803195592b435
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-06-16 11:48:15 +0530

    [Shreyash Singh] .Add StudentValidAge Completed

---
+++ Array/StudentValidAge.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class StudentValidAge {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       int arr[]=new int[10];
+       for(int i=0;i<10;i++){
+        arr[i]=sc.nextInt();
+       }
+       for(int i=0;i<arr.length;i++){
+if(arr[i]<0){
+    System.out.println("Invalid Age");
+}
+else if(arr[i]>0 && arr[i]<18){
+    System.out.println("The Student with age "+ arr[i]+" can not vote");
+}
+else{
+    System.out.println("The Student with age "+ arr[i]+" can vote");
+}
+       }
+return;
+    }
+}
