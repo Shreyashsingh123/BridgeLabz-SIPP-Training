943823de641f931637e88ee0d148b0ee4d0d0f33
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-06-17 10:09:02 +0530

    [Shreyash Singh] .Add FreindAgeComparison Completed

---
+++ Array/FreindAgeComparison.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+public class FreindAgeComparison {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+String[] names = {"Amar", "Akbar", "Anthony"};
+int[] ages = new int[3];
+double[] heights = new double[3];
+for (int i = 0; i < 3; i++) {
+ages[i] = sc.nextInt();
+heights[i] = sc.nextDouble();
+}
+int youngestIndex = 0;
+for (int i = 1; i < 3; i++) {
+if (ages[i] < ages[youngestIndex]) {
+youngestIndex = i;
+}
+}
+int tallestIndex = 0;
+for (int i = 1; i < 3; i++) {
+if (heights[i] > heights[tallestIndex]) {
+tallestIndex = i;
+}
+}
+System.out.println("Youngest friend is " + names[youngestIndex] + " with age " + ages[youngestIndex]);
+System.out.println("Tallest friend is " + names[tallestIndex] + " with height " + heights[tallestIndex]);
+}
+}
