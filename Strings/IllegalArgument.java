3d780c06940d38ab5e323b7ffb496b725e484abf
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-06-18 11:46:31 +0530

    [Shreyash Singh] .Add IllegalArgument  Completed

---
+++ Strings/IllegalArgument.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class IllegalArgument {
+public static void generateException(String text) {
+System.out.println(text.substring(5, 2));
+}
+public static void handleException(String text) {
+try {
+System.out.println(text.substring(5, 2));
+} catch (IllegalArgumentException e) {
+System.out.println("Caught IllegalArgumentException: " + e.getMessage());
+} catch (RuntimeException e) {
+System.out.println("Caught RuntimeException: " + e.getMessage());
+}
+}
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter a string: ");
+String input = sc.nextLine();
+// generateException(input);
+handleException(input);
+}
+}
+
+
