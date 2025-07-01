81db4d20d341d2cc20d0c951561ad63b73a61ab5
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-07-01 11:21:59 +0530

    [Shreyash Singh] fix . Fix the conflict between OOPS & Core Java

--- Java Programming Elements/AverageofSam.java
+++
@@ -1,9 +0,0 @@
-class AverageofSam{
-    public static void main(String[] args) {
-        int math = 94;
-        int physics = 95;
-        int chem = 96;
-        double average = (math + physics + chem) / 3.0;
-        System.out.println("Sam’s average mark in PCM is " + average);
-    }
-}
