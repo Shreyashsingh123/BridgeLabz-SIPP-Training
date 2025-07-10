2d5ad1452d38fa7f0dac7cce2e892248c0a33f6f
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-07-10 10:04:41 +0530

    [Shreyash Singh]  .Add Salary Sorting by Heap   Sort Completed

---
+++ Sorting/SalarySorting.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SalarySorting {
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0]; arr[0] = arr[i]; arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i, left = 2*i + 1, right = 2*i + 2;
+
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        if (right < n && arr[right] > arr[largest]) largest = right;
+
+        if (largest != i) {
+            int temp = arr[i]; arr[i] = arr[largest]; arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of salary demands: ");
+        int n = sc.nextInt();
+        int[] salaries = new int[n];
+        System.out.println("Enter the salaries:");
+        for (int i = 0; i < n; i++) salaries[i] = sc.nextInt();
+
+        heapSort(salaries);
+
+        System.out.println("Sorted Salaries:");
+        for (int s : salaries) System.out.print(s + " ");
+    }
+}
