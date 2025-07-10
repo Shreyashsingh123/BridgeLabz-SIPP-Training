82dc445d4b4827f4eaeba5635b71cab3fc32bc7e
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-07-10 09:59:02 +0530

    [Shreyash Singh]  .Add Student Age sorting by Counting  Sort Completed

---
+++ Sorting/StudentAgeSorting.java
@@ -0,0 +1,37 @@
+  import java.util.*;
+    public class StudentAgeSorting {
+        public static void countingSort(int[] arr) {
+            int max = 18, min = 10;
+            int range = max - min + 1;
+    
+            int[] count = new int[range];
+            int[] output = new int[arr.length];
+            for (int age : arr) count[age - min]++;
+    
+            for (int i = 1; i < count.length; i++)
+                count[i] += count[i - 1];
+    
+            for (int i = arr.length - 1; i >= 0; i--) {
+                output[count[arr[i] - min] - 1] = arr[i];
+                count[arr[i] - min]--;
+            }
+    
+            for (int i = 0; i < arr.length; i++) arr[i] = output[i];
+        }
+    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter number of student: ");
+            int n = sc.nextInt();
+            int[] ages = new int[n];
+            System.out.println("Enter the ages (10 to 18):");
+            for (int i = 0; i < n; i++) {
+                ages[i] = sc.nextInt();
+            }
+            countingSort(ages);
+    
+            System.out.println("Sorted Ages:");
+            for (int age : ages) System.out.print(age + " ");
+        }
+    }
+    
