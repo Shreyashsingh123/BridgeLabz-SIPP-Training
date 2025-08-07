08e625714cd53499b6aa037606c70c22fcfe2307
Shreyashsingh123 <singhshreyash631@gmail.com>
2025-08-07 10:16:31 +0530

    Regex Completed

--- Java Programming Elements/AgeOfHarry.java
+++
@@ -1,8 +0,0 @@
-public class AgeOfHarry{
-public static void main(String[] args){
-int birthyear=2000;
-int curryear=2024;
-int age=curryear-birthyear;
-System.out.print("harry's age in 2024 is"+age);
-}
-}
